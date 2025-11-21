e2825ba04183370b102257213a8114557e5dd440
KittenFlood <brighella@gmail.com>
2025-11-21 14:53:17 +0100

    change build outDir, fix header layout

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/joinery-sim/',
+  build: {
+    outDir: 'docs',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
