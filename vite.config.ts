fb0ce2d78863dc15cef400bf418776f82a3724da
KittenFlood <brighella@gmail.com>
2025-11-21 11:15:26 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/joinery-sim/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
-export default {
-  base: '/joinery-sim/'
-};
-
